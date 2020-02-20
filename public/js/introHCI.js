dfd54242aa01a076a360c41df0a1aaf10b360ddd
Yuhan Zhang <yuz053@ucsd.edu>
2020-02-20 02:35:55 -0800

    lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,9 +5,19 @@ $(document).ready(function() {
 	initializePage();
 });
 
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(liked);
 }
+
+function liked(e){
+	e.preventDefault();
+	console.log("clicked")
+	ga('create', 'UA-158825803-1', 'auto');
+	ga('send', 'event', 'like', 'click');
+}
+
